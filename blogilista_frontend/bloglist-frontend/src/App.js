113da87c0535d92919494805e964bdb6d1001da3
Maria Larionova <maria.larionova@helsinki.fi>
2019-04-15 16:50:51 +0300

    tehtävä 7.8

--- blogilista_frontend/bloglist-frontend/src/App.js
+++ blogilista_frontend/bloglist-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import userService from './services/users'
 import loginService from './services/login'
 import LoginForm from './components/loginForm'
 import BlogForm from './components/blogForm'
@@ -12,18 +13,105 @@ import {
   setNotification,
   clearNotification
 } from './reducers/notificationReducer'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-const App = (props) => {
-  const [blogs, setBlogs] = useState([])
-  const [user, setUser] = useState(null)
-  const username = useField('text')
-  const password = useField('password')
+const BlogList = props => {
   const title = useField('text')
   const author = useField('text')
   const url = useField('text')
 
+  const blogForm = () => (
+    <Togglable buttonLabel="Create new blog">
+      <BlogForm author={author} title={title} url={url} addBlog={addBlog} />
+    </Togglable>
+  )
+  const addBlog = event => {
+    event.preventDefault()
+    const blogObject = {
+      title: title.value,
+      author: author.value,
+      url: url.value,
+      likes: 0
+    }
+    blogService.create(blogObject).then(returnedBlog => {
+      props.setBlogs(props.blogs.concat(returnedBlog))
+      author.props.reset()
+      title.props.reset()
+      url.props.reset()
+    })
+    props.setNotification('Blogin lisääminen onnistui', 5)
+  }
+
+  const addLike = blog => {
+    const blogObject = {
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: blog.likes + 1,
+      id: blog.id
+    }
+
+    blogService.update(blogObject).then(allBlogs => {
+      props.setBlogs(props.blogs.map(b => (b.id === blog.id ? allBlogs : b)))
+    })
+
+    props.setNotification(`Blogiin "${blogObject.title}" on lisätty tykkäys`, 5)
+  }
+
+  const sortBlogs = blogs => {
+    blogs.sort(function(a, b) {
+      return b.likes - a.likes
+    })
+    return blogs
+  }
+
+  const deleteBlog = blog => {
+    const blogObject = {
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: blog.likes,
+      id: blog.id
+    }
+
+    blogService
+      .remove(blogObject)
+      .then(
+        props.setBlogs(props.blogs.filter(allBlogs => allBlogs.id !== blog.id))
+      )
+
+    props.setNotification(`Blogi "${blogObject.title}" on poistettu`, 5)
+  }
+  return (
+    <div>
+      <h2>Blogs</h2>
+      {blogForm()}
+      <div>
+        <p />
+      </div>
+      {sortBlogs(props.blogs).map(blog => (
+        <Blog
+          key={blog.id}
+          blog={blog}
+          addLike={addLike}
+          user={props.user}
+          deleteBlog={deleteBlog}
+        />
+      ))}
+    </div>
+  )
+}
+
+const App = props => {
+  const [blogs, setBlogs] = useState([])
+  const [users, setUsers] = useState([])
+  const [user, setUser] = useState(null)
+  const username = useField('text')
+  const password = useField('password')
+
   useEffect(() => {
     blogService.getAll().then(blogs => setBlogs(blogs))
+    userService.getAll().then(users => setUsers(users))
   }, [])
 
   useEffect(() => {
@@ -51,7 +139,6 @@ const App = (props) => {
       username.props.reset()
 
       props.setNotification('Kirjautuminen onnistui', 5)
-
     } catch (exception) {
       props.setNotification('Käyttäjätunnus tai salasana virheellinen', 5)
     }
@@ -66,70 +153,9 @@ const App = (props) => {
     </Togglable>
   )
 
-  const blogForm = () => (
-    <Togglable buttonLabel="Create new blog">
-      <BlogForm author={author} title={title} url={url} addBlog={addBlog} />
-    </Togglable>
-  )
-
   const logout = () => {
     setUser(null)
     window.localStorage.clear()
-  }
-  const addBlog = event => {
-    event.preventDefault()
-    const blogObject = {
-      title: title.value,
-      author: author.value,
-      url: url.value,
-      likes: 0
-    }
-    blogService.create(blogObject).then(returnedBlog => {
-      setBlogs(blogs.concat(returnedBlog))
-      author.props.reset()
-      title.props.reset()
-      url.props.reset()
-    })
-    props.setNotification('Blogin lisääminen onnistui', 5)
-  }
-
-  const addLike = blog => {
-    const blogObject = {
-      title: blog.title,
-      author: blog.author,
-      url: blog.url,
-      likes: blog.likes + 1,
-      id: blog.id
-    }
-
-    blogService.update(blogObject).then(allBlogs => {
-      setBlogs(blogs.map(b => (b.id === blog.id ? allBlogs : b)))
-    })
-
-    props.setNotification(`Blogiin "${blogObject.title}" on lisätty tykkäys`, 5)
-
-  }
-  const sortBlogs = blogs => {
-    blogs.sort(function(a, b) {
-      return b.likes - a.likes
-    })
-    return blogs
-  }
-
-  const deleteBlog = blog => {
-    const blogObject = {
-      title: blog.title,
-      author: blog.author,
-      url: blog.url,
-      likes: blog.likes,
-      id: blog.id
-    }
-
-    blogService
-      .remove(blogObject)
-      .then(setBlogs(blogs.filter(allBlogs => allBlogs.id !== blog.id)))
-
-    props.setNotification(`Blogi "${blogObject.title}" on poistettu`, 5)
   }
 
   return (
@@ -144,19 +170,85 @@ const App = (props) => {
         <div>
           <p>{user.name} logged in</p>
           <button onClick={() => logout()}>logout</button>
-          {blogForm()}
-          <h2>Blogs</h2>
-          {sortBlogs(blogs).map(blog => (
-            <Blog
-              key={blog.id}
-              blog={blog}
-              addLike={addLike}
-              user={user}
-              deleteBlog={deleteBlog}
-            />
-          ))}
+          {users.length > 0 && <Users users={users} />}
+
+          <Menu blogs={blogs} users={users} user={user} setBlogs={setBlogs} />
         </div>
       )}
+    </div>
+  )
+}
+
+const Users = props => {
+  console.log(props.users)
+  return (
+    <div>
+      <h2>Users</h2>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <Link to="/">Kaikki blogit</Link>
+            </td>
+            <td>
+              <b>Blogs</b>
+            </td>
+          </tr>
+          {props.users.map(user => (
+            <tr key={user.id}>
+              <td>
+                <Link to={`/users/${user.id}`}>{user.name}</Link>
+              </td>
+              <td>{user.blogs.length}</td>
+            </tr>
+          ))}
+
+          {/* <td>{props.users.reduce((acc,user) => acc + user.blogs.length, 0)}</td> */}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const UserView = props => {
+  console.log('props in blogs', props)
+  if (!props.users.length) {
+    return null
+  }
+
+  // käyttäjää ei löydy
+  if (!props.users.find(({ id }) => id === props.id)) {
+    return <h1>VIRHE!!! RIKOS!! Käyttäjää ei löytynyt</h1>
+  }
+
+  return (
+    <div>
+      <h2>{props.users.find(({ id }) => id === props.id).name}'s</h2>
+      <h3>Added blogs</h3>
+      <ul>
+        {props.blogs
+          .filter(b => b.user.id === props.id)
+          .map(b => <li key={b.id}>{b.title}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+const Menu = props => {
+  return (
+    <div>
+      <Route exact path="/" render={() => <BlogList {...props} />} />
+      <Route
+        exact
+        path="/users/:id"
+        render={({ match }) => (
+          <UserView
+            blogs={props.blogs}
+            users={props.users}
+            id={match.params.id}
+          />
+        )}
+      />
     </div>
   )
 }
@@ -166,4 +258,14 @@ const mapDispatchToProps = {
   clearNotification
 }
 
-export default connect(null, mapDispatchToProps)(App)
+const Wrapper = props => {
+  return (
+    <div>
+      <Router>
+        <App {...props} />
+      </Router>
+    </div>
+  )
+}
+
+export default connect(null, mapDispatchToProps)(Wrapper)
